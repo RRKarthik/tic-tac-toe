3f8bb51952c49ad1d3640f3ce75461ed031e2e42
RRKarthik <58585694+RRKarthik@users.noreply.github.com>
2021-04-11 12:33:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,94 @@
+const gameBoard = (function()
+{
+    const arr = [
+         [0,0,0],
+         [0,0,0],
+         [0,0,0]
+
+        ],
+
+    marker = function(e)
+        {
+           
+             if(gameBoard.arr[e.target.getAttribute("data-row")][e.target.getAttribute("data-col")] != 'x' && gameBoard.arr[e.target.getAttribute("data-row")][e.target.getAttribute("data-col")] != 'o')
+              {
+                 if(turn)
+                {
+                 para.innerHTML = `${playerTwo.playerName}'s Turn`;   
+                 e.target.innerHTML = playerOne.symbol;
+                 gameBoard.arr[e.target.getAttribute("data-row")][e.target.getAttribute("data-col")] = playerOne.symbol;
+                 turn = !turn
+                 count = count + 1;
+                 gameBoard.checker(playerOne.playerName,playerOne.symbol,e.target.getAttribute("data-row"),e.target.getAttribute("data-col"));
+                }else{
+                 para.innerHTML = `${playerOne.playerName}'s Turn`;
+                 e.target.innerHTML = playerTwo.symbol;
+                 gameBoard.arr[e.target.getAttribute("data-row")][e.target.getAttribute("data-col")] = playerTwo.symbol;
+                 turn = !turn
+                 count = count + 1;
+                 gameBoard.checker(playerTwo.playerName,playerTwo.symbol,e.target.getAttribute("data-row"),e.target.getAttribute("data-col"));
+                 }
+             }
+    
+        },
+
+
+
+    checker = function(name,symbol,row,col)
+    {
+   
+   
+       if(gameBoard.arr[row][0] == symbol &&  gameBoard.arr[row][1] == symbol && gameBoard.arr[row][2] == symbol){
+           para.innerHTML = `${name} wins`;
+           document.getElementById("container").style.display = "none";
+           
+       }else if(gameBoard.arr[0][col] == symbol &&  gameBoard.arr[1][col] == symbol && gameBoard.arr[2][col] == symbol)
+       {
+        para.innerHTML = `${name} wins`;
+        document.getElementById("container").style.display = "none";
+        
+       }else if(gameBoard.arr[0][0] == symbol &&  gameBoard.arr[1][1] == symbol && gameBoard.arr[2][2] == symbol)
+       {
+        para.innerHTML = `${name} wins`;
+        document.getElementById("container").style.display = "none";
+        
+       }else if(gameBoard.arr[0][2] == symbol && gameBoard.arr[1][1] == symbol && gameBoard.arr[2][0] == symbol){
+            para.innerHTML = `${name} wins`;
+            document.getElementById("container").style.display = "none";
+           
+        }
+         if(count == 9)
+         {
+          para.innerHTML = "Its a tie";
+          document.getElementById("container").style.display = "none";
+         }
+   }
+    return {arr,checker,marker};
+})();
+
+const createPlayer = function(name,symbol){
+    playerName = name;
+    symbol = symbol;
+    return{ playerName , symbol};
+
+};
+
+const playerOne = createPlayer('player 1','x');
+const playerTwo = createPlayer('player 2','o');
+
+let count = 0;
+const para = document.getElementById("score");
+let turn = true;
+const blocks = document.querySelectorAll(".block");
+blocks.forEach( ele => ele.addEventListener("click",gameBoard.marker));
+
+document.querySelector("#btn").addEventListener("click",refresher);
+function refresher()
+{
+    location.reload();
+}
+
+
+
+
+
